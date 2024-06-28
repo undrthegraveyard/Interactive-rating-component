ce4b3360a81be779670ba7f82a2100ff997d407b
Shivam <shivamagarwalmarch@gmail.com>
2024-06-28 15:07:36 +1000

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ratingOptions = document.querySelectorAll('.rating-option');
+  const submitButton = document.querySelector('button[aria-label="submit"]');
+  const ratingState = document.querySelector('.rating-state');
+  const thankyouState = document.querySelector('.thank-you-state');
+  let selectedRatingSpan = document.getElementById('selected-rating');
+
+  let selectedRating =0;
+
+  function 
+});
